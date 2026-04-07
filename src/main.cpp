57cb7d7a6c0ca4e947262d6bc3801a116b703508
MaciejGit6 <macie.milewski@gmail.com>
2026-04-07 17:45:13 +0200

    Logo added

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 
-int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[]) {
-    std::cout << "[*] Scipio binary analysis tool initialized." << std::endl;
-    
+void print_banner() {
+    std::cout << R"(
+   _____  _____ _____ _____ _____  ____  
+  / ____|/ ____|_   _|  __ \_   _|/ __ \ 
+ | (___ | |      | | | |__) || | | |  | |
+  \___ \| |      | | |  ___/ | | | |  | |
+  ____) | |____ _| |_| |    _| |_| |__| |
+ |_____/ \_____|_____|_|   |_____|\____/ 
+                                         
+    )" << '\n';
+    std::cout << "[*] Scipio Binary Analysis Tool - v0.1\n";
+    std::cout << "[*] Initializing decoder engine...\n\n";
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
+    print_banner();
     
     return 0;
 }
