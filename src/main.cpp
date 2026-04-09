80b9a4e425f2bc6537f1e531fd1a207d1ed29a9f
MaciejGit6 <macie.milewski@gmail.com>
2026-04-09 23:13:26 +0200

    fixing removing tracked files that are in .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ extern "C" void instruction_receiver(uint64_t address, const char* mnemonic, con
 
 
 void print_banner() {
-    std::cout << R"(
+    /*std::cout << R"(
    _____  _____ _____ _____ _____  ____  
   / ____|/ ____|_   _|  __ \_   _|/ __ \ 
  | (___ | |      | | | |__) || | | |  | |
@@ -61,7 +61,7 @@ void print_banner() {
   ____) | |____ _| |_| |    _| |_| |__| |
  |_____/ \_____|_____|_|   |_____|\____/ 
                                          
-    )" << '\n';
+    )" << '\n';*/
     std::cout << "[*] Scipio Binary Analysis Tool - v0.1\n";
     std::cout << "[*] Initializing decoder engine...\n\n";
 }
