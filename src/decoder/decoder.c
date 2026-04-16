9fdd1525e06755a2a29351d93b20ab6d0036b03e
MaciejGit6 <macie.milewski@gmail.com>
2026-04-16 23:40:21 +0200

    extract_branch_target impl

--- src/decoder/decoder.c
+++ src/decoder/decoder.c
@@ -1,5 +1,6 @@
 #include "decoder.h"
 #include <string.h>
+#include <stdlib.h>
 
 InsnType classify_instruction(const char* mnemonic){
     if(strcmp(mnemonic, "ret") == 0 || strcmp(mnemonic, "retq") == 0)
@@ -17,4 +18,19 @@ InsnType classify_instruction(const char* mnemonic){
     return INSN_NORMAL;
 
             
+}
+
+static uint64_t extract_branch_target(const char* operands) {
+    const char* hex = strstr(operands, "0x");
+    if (hex == NULL)
+        return 0;
+
+    char* end;
+    uint64_t target = strtoull(hex, &end, 16);
+
+  
+    if (end == hex)
+        return 0;
+
+    return target;
 }
