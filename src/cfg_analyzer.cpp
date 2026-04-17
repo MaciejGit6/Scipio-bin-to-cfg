66845f8f8543f9d54c2b846b65f70eb83f3b360f
MaciejGit6 <macie.milewski@gmail.com>
2026-04-17 19:47:25 +0200

    BFD reachability pass cfg_analyzer.cpp

---
+++ src/cfg_analyzer.cpp
@@ -0,0 +1,24 @@
+#include "cfg_analyzer.hpp"
+#include <queue>
+
+std::unordered_set<uint64_t> CFGAnalyzer::bfs_reachable() const{
+    std::unordered_set<uint64_t> visited;
+    std::queue<uint64_t> worklist;
+
+    worklist.push(entry_);
+    visited.insert(entry_);
+
+    while(!worklist.empty()){
+        uint64_t addr = worklist.front();
+        worklist.pop();
+
+        auto block = cfg_.get_block(addr);
+        if(!block) continue;
+
+        for(const auto& succ : block->successors){
+            if(visited.insert(succ->start_address).second)
+                worklist.push(succ->start_address);
+        }
+    }
+    return visited;
+}
